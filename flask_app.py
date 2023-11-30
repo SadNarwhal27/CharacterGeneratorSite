404932bcb7b4d98a2dca42022b08779686104d11
SadNarwhal27 <schallt2015@gmail.com>
2023-11-30 00:19:48 -0500

    Created item cards and refactored a lot

--- flask_app.py
+++ flask_app.py
@@ -13,6 +13,7 @@ def create_npc():
     race = request.form.get('race')
     gender = request.form.get('gender')
     backstory = request.form.get('backstory')
+    print(race, gender, backstory)
 
     # Production
     response = requests.get(os.getenv('API_URL') + f"/npc?race={race}&gender={gender}&backstory={backstory}")
@@ -21,6 +22,19 @@ def create_npc():
     # response = requests.get('http://127.0.0.1:5001/' + f"/npc?race={race}&gender={gender}&backstory={backstory}")
 
     return render_template('npc_created.html', response=response.json())
+
+@app.route('/item', methods=['GET','POST'])
+def create_item():
+    treasure_type = request.form.get('type')
+    description_lod = request.form.get('lod')
+
+    # Production
+    response = requests.get(os.getenv('API_URL') + f"/item?type={treasure_type}&lod={description_lod}")
+
+    # For testing
+    # response = requests.get('http://127.0.0.1:5001/' + f"/item?type={treasure_type}&lod={description_lod}")
+    
+    return render_template('item_created.html', response=response.json())
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
---
+++ static/scripts.js
@@ -0,0 +1,3 @@
+function showSpinner(spinner_id) {
+    document.getElementById(spinner_id).innerHTML = '<div style="color: white;" class="spinner-border"></div>';
+}
